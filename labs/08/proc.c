718f362b69f1410da91b1e910d6bb28cbc217e0e
Diego <diegomzdedios@gmail.com>
2019-10-27 07:19:52 +0000

    A01228042-homework-06
    Signed-off-by: Diego <diegomzdedios@gmail.com>

---
+++ labs/08/proc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int proc(int a[]) {
+	int sum = 0, i;
+	for (i=0; i < 1000000; i++)
+		sum += a[i];
+	return sum;
+}
+
+int main(){
+    int sum;
+    int* arr =  (int*)malloc(sizeof(int)*1000000);
+    sum = proc(arr);
+    printf("Sum: %d\n",sum);
+    return 0;
+}
